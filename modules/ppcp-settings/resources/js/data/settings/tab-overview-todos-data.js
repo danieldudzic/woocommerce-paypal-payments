47b9cfbbdb70acf787590c8a46fa6008bc715f10
Emili Castells Guasch <e.castells@inpsyde.com>
2025-01-03 17:41:05 +0100

    Add todos data

--- modules/ppcp-settings/resources/js/data/settings/tab-overview-todos-data.js
+++ modules/ppcp-settings/resources/js/data/settings/tab-overview-todos-data.js
@@ -50,4 +50,121 @@ export const todosData = [
 			selectTab( TAB_IDS.OVERVIEW, 'pay_later_messaging' );
 		},
 	},
+	{
+		id: 'configure_paypal_subscription',
+		title: __(
+			'Configure a PayPal Subscription',
+			'woocommerce-paypal-payments'
+		),
+		description: __(
+			'Connect a subscriptions-type product from WooCommerce with PayPal.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			console.log(
+				'Take merchant to product list, filtered with subscription-type products'
+			);
+		},
+	},
+	{
+		id: 'register_domain_apple_pay',
+		title: __(
+			'Register Domain for Apple Pay',
+			'woocommerce-paypal-payments'
+		),
+		description: __(
+			'To enable Apple Pay, you must register your domain with PayPal.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			selectTab( TAB_IDS.OVERVIEW, 'apple_pay' );
+		},
+	},
+	{
+		id: 'add_digital_wallets_to_account',
+		title: __(
+			'Add digital wallets to your account',
+			'woocommerce-paypal-payments'
+		),
+		description: __(
+			'Add the ability to accept Apple Pay & Google Pay to your PayPal account.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			console.log(
+				'Take merchant to PayPal to enable Apple Pay & Google Pay'
+			);
+		},
+	},
+	{
+		id: 'add_apple_pay_to_account',
+		title: __(
+			'Add Apple Pay to your account',
+			'woocommerce-paypal-payments'
+		),
+		description: __(
+			'Add the ability to accept Apple Pay to your PayPal account.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			console.log( 'Take merchant to PayPal to enable Apple Pay' );
+		},
+	},
+	{
+		id: 'add_google_pay_to_account',
+		title: __(
+			'Add Google Pay to your account',
+			'woocommerce-paypal-payments'
+		),
+		description: __(
+			'Add the ability to accept Google Pay to your PayPal account.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			console.log( 'Take merchant to PayPal to enable Google Pay' );
+		},
+	},
+	{
+		id: 'enable_apple_pay',
+		title: __( 'Enable Apple Pay', 'woocommerce-paypal-payments' ),
+		description: __(
+			'Allow your buyers to check out via Apple Pay.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			selectTab( TAB_IDS.OVERVIEW, 'apple_pay' );
+		},
+	},
+	{
+		id: 'enable_google_pay',
+		title: __( 'Enable Google Pay', 'woocommerce-paypal-payments' ),
+		description: __(
+			'Allow your buyers to check out via Google Pay.',
+			'woocommerce-paypal-payments'
+		),
+		isCompleted: () => {
+			return false;
+		},
+		onClick: () => {
+			selectTab( TAB_IDS.OVERVIEW, 'google_pay' );
+		},
+	},
 ];
