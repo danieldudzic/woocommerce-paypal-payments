efa4ca1a43063de6d8c42a806202dee59881a992
Philipp Stracker <p.stracker@syde.com>
2025-01-10 13:15:11 +0100

    ♻️ Convert SVG logo to Icon component

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/Icons.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/Icons.js
@@ -1,3 +1,5 @@
 export { default as openSignup } from './Icons/open-signup';
+export { default as logoPayPal } from './Icons/logo-paypal';
+
 export const NOTIFICATION_SUCCESS = '✔️';
 export const NOTIFICATION_ERROR = '❌';
--- modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Components/OnboardingHeader.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Onboarding/Components/OnboardingHeader.js
@@ -1,11 +1,13 @@
-import data from '../../../../utils/data';
+import { Icon } from '@wordpress/components';
+
+import { logoPayPal } from '../../../ReusableComponents/Icons';
 
 const OnboardingHeader = ( props ) => {
 	return (
 		<section className="ppcp-r-onboarding-header">
 			<div className="ppcp-r-onboarding-header__logo">
 				<div className="ppcp-r-onboarding-header__logo-wrapper">
-					{ data().getImage( 'logo-paypal.svg' ) }
+					<Icon icon={ logoPayPal } width="auto" height={ 38 } />
 				</div>
 			</div>
 			<div className="ppcp-r-onboarding-header__content">
