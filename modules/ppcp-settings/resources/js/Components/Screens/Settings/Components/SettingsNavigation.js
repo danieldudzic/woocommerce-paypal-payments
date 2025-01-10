47be154307295b6a1de69cd69a5d3459130e3f00
Philipp Stracker <p.stracker@syde.com>
2025-01-10 15:14:15 +0100

    ♻️ Refactor Settings to use new TopNavigation

--- modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/SettingsNavigation.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/SettingsNavigation.js
@@ -1,36 +1,17 @@
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import data from '../../../../utils/data';
 
-const SettingsNavigation = ( {} ) => {
+import TopNavigation from '../../../ReusableComponents/TopNavigation';
+
+const SettingsNavigation = () => {
+	const title = __( 'PayPal Payments', 'woocommerce-paypal-payments' );
+
 	return (
-		<div className="ppcp-r-navigation-container">
-			<div className="ppcp-r-navigation">
-				<div className="ppcp-r-navigation--left">
-					<span>{ data().getImage( 'icon-arrow-left.svg' ) }</span>
-					<a
-						className="ppcp-r-navigation--left__link"
-						href={ global.ppcpSettings.wcPaymentsTabUrl }
-						aria-label={ __(
-							'PayPal Payments',
-							'woocommerce-paypal-payments'
-						) }
-					>
-						{ __(
-							'PayPal Payments',
-							'woocommerce-paypal-payments'
-						) }
-					</a>
-				</div>
-				{
-					<div className="ppcp-r-navigation--right">
-						<Button variant="primary" disabled={ false }>
-							{ __( 'Save', 'woocommerce-paypal-payments' ) }
-						</Button>
-					</div>
-				}
-			</div>
-		</div>
+		<TopNavigation title={ title } exitOnTitleClick={ true }>
+			<Button variant="primary" disabled={ false }>
+				{ __( 'Save', 'woocommerce-paypal-payments' ) }
+			</Button>
+		</TopNavigation>
 	);
 };
 
