c2c4347ce2491ab9faec1b2bcf55d3a155806e3a
carmenmaymo <mayen22@gmail.com>
2025-01-13 15:21:36 +0100

    use merchant directly

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/ConnectionInfo.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/ConnectionInfo.js
@@ -1,17 +1,8 @@
 import { __ } from '@wordpress/i18n';
-import { useState, useEffect } from '@wordpress/element';
-import {CommonHooks} from "../../data";
-
+import { CommonHooks } from '../../data';
 
 const ConnectionInfo = () => {
-    const { merchant } = CommonHooks.useMerchantInfo();
-    const [connectionData, setConnectionData] = useState(getDefaultConnectionStatusData(merchant));
-
-    useEffect(() => {
-        if (merchant) {
-            setConnectionData(getDefaultConnectionStatusData(merchant));
-        }
-    }, [merchant]);
+	const { merchant } = CommonHooks.useMerchantInfo();
 
 	return (
         <div className="ppcp-r-connection-status__data">
@@ -31,16 +22,6 @@ const ConnectionInfo = () => {
     );
 };
 export default ConnectionInfo;
-export const getDefaultConnectionStatusData = (merchant = null) => {
-    if (!merchant) {
-        const contextMerchant = CommonHooks.useMerchantInfo()?.merchant || {};
-        return {
-            connectionStatus: contextMerchant.isConnected || false,
-            email: contextMerchant.email || '',
-            merchantId: contextMerchant.id || '',
-            clientId: contextMerchant.clientId || '',
-        };
-    }
 
     return {
         connectionStatus: merchant.isConnected || false,
