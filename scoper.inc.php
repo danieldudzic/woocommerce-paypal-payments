37384ce8e5670abb36f1cadceb2428b615803988
Narek Zakarian <narekzakarian@gmail.com>
2025-01-15 13:17:32 +0400

    Exclude files from root directory

--- scoper.inc.php
+++ scoper.inc.php
@@ -10,28 +10,19 @@ declare(strict_types=1);
 use Isolated\Symfony\Component\Finder\Finder;
 
 return array(
-	'prefix'             => 'WooCommerce\\PayPalCommerce\\Vendor',
-	'finders'            => array(
+	'prefix'                  => 'WooCommerce\\PayPalCommerce\\Vendor',
+	'finders'                 => array(
 		// Include only the Psr\Log package for scoping.
 		Finder::create()
 			->files()
 			->in( __DIR__ . '/vendor/psr/log' ),
-
-		// Exclude all other project files from scoping.
-		Finder::create()
-			->files()
-			->in( __DIR__ )
-			->exclude( 'vendor' ),
 	),
-	'expose-classes'     => array(
-		// Expose all project namespaces to prevent scoping.
-		'*',
+	'exclude-files'           => array(
+		// Exclude all files in the root directory explicitly.
+		__DIR__ . '/*.php',
 	),
-	'exclude-namespaces' => array(
-		// Do not exclude any namespace to allow Psr\Log to be scoped.
-	),
-	'exclude-files'      => array(
-		// Optionally exclude specific files, if required.
-	),
-	'patchers'           => array(),
+	'expose-classes'          => array( '*' ), // Expose all classes outside `vendor/psr/log`.
+	'expose-namespaces'       => array( '*' ), // Expose all namespaces except `Psr\Log`.
+	'expose-global-constants' => true,
+	'expose-global-functions' => true,
 );
