6c5254dc94eeeaa255cc1cd3b66fec4e2e79e9a8
Narek Zakarian <narekzakarian@gmail.com>
2025-01-15 17:17:07 +0400

    try to exclude root files

--- scoper.inc.php
+++ scoper.inc.php
@@ -30,7 +30,10 @@ return array(
 	'prefix'                  => 'WooCommerce\\PayPalCommerce\\Vendor',
 	'finders'                 => $finders,
 	'patchers'                => array(),
-	'exclude-files'           => array( '/uninstall.php' ), // list<string>.
+	'exclude-files'           => array(
+		// Explicitly exclude the root files or any other files that should not be scoped.
+		__DIR__ . '/*.php',
+	), // list<string>.
 	'exclude-namespaces'      => array(
 		'/^(?!Psr).*/',
 	), // list<string|regex>.
