c9d6a15134b780de469546206986310fb92d202f
Narek Zakarian <narekzakarian@gmail.com>
2025-01-15 16:35:20 +0400

    Use regexp

--- scoper.inc.php
+++ scoper.inc.php
@@ -38,7 +38,7 @@ return array(
 		'vendor/symfony/polyfill-php80/Resources/stubs/Stringable.php',
 	), // list<string>.
 	'exclude-namespaces'      => array(
-		'^(?!Psr).*',
+		'/^(?!Psr).*/',
 	), // list<string|regex>.
 	'exclude-constants'       => array_merge(
 		$wp_constants,
