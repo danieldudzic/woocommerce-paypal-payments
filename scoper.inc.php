cb454a32c0d36e92f6964a25c439769fce507728
Narek Zakarian <narekzakarian@gmail.com>
2025-01-15 17:04:02 +0400

    Use regexp

--- scoper.inc.php
+++ scoper.inc.php
@@ -9,10 +9,6 @@ declare(strict_types=1);
 
 use Isolated\Symfony\Component\Finder\Finder;
 
-$wp_classes   = array();
-$wp_constants = array();
-$wp_functions = array();
-
 $finders = array(
 	Finder::create()
 		->files()
@@ -34,31 +30,13 @@ return array(
 	'prefix'                  => 'WooCommerce\\PayPalCommerce\\Vendor',
 	'finders'                 => $finders,
 	'patchers'                => array(),
-	'exclude-files'           => array(
-		'vendor/symfony/polyfill-php80/Resources/stubs/Stringable.php',
-	), // list<string>.
+	'exclude-files'           => array( '/uninstall.php' ), // list<string>.
 	'exclude-namespaces'      => array(
 		'/^(?!Psr).*/',
 	), // list<string|regex>.
-	'exclude-constants'       => array_merge(
-		$wp_constants,
-		array(
-			'WC_VERSION',
-		)
-	), // list<string|regex>.
-	'exclude-classes'         => array_merge(
-		$wp_classes,
-		array(
-			'WooCommerce',
-			'/^WC_/',
-		)
-	),     // list<string|regex>.
-	'exclude-functions'       => array_merge(
-		$wp_functions,
-		array(
-			'/^wc/',
-		)
-	), // list<string|regex>.
+	'exclude-constants'       => array(), // list<string|regex>.
+	'exclude-classes'         => array_merge(),     // list<string|regex>.
+	'exclude-functions'       => array_merge(), // list<string|regex>.
 
 	'expose-global-constants' => false,   // bool.
 	'expose-global-classes'   => false,     // bool.
