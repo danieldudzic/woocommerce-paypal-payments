0a9d5a5dbe58ffb3b96738ca1100e89c070afef6
Narek Zakarian <narekzakarian@gmail.com>
2025-01-15 14:38:55 +0400

    include only psr/log

--- scoper.inc.php
+++ scoper.inc.php
@@ -10,16 +10,28 @@ declare(strict_types=1);
 use Isolated\Symfony\Component\Finder\Finder;
 
 return array(
-	'prefix'                  => 'WooCommerce\\PayPalCommerce\\Vendor',
-	'finders'                 => array(
-		// Only include files from the psr/log package for scoping.
+	'prefix'             => 'WooCommerce\\PayPalCommerce\\Vendor',
+	'finders'            => array(
+		// Include only the Psr\Log package for scoping.
 		Finder::create()
 			->files()
-			->in( __DIR__ . '/vendor/psr/log' ), // Only include the 'psr/log' directory.
+			->in( __DIR__ . '/vendor/psr/log' ),
+
+		// Exclude all other project files from scoping.
+		Finder::create()
+			->files()
+			->in( __DIR__ )
+			->exclude( 'vendor' ),
 	),
-	'exclude-files'           => array(
-		__DIR__ . '/api/*', // Optionally, exclude other files you do not want to scope.
+	'expose-classes'     => array(
+		// Expose all project namespaces to prevent scoping.
+		'*',
 	),
-	'expose-global-constants' => true,
-	'expose-global-functions' => true,
+	'exclude-namespaces' => array(
+		// Do not exclude any namespace to allow Psr\Log to be scoped.
+	),
+	'exclude-files'      => array(
+		// Optionally exclude specific files, if required.
+	),
+	'patchers'           => array(),
 );
