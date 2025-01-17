e00b95fd1d7a061d07ff72dc055f98eae3c87b7d
Philipp Stracker <p.stracker@syde.com>
2025-01-17 16:24:49 +0100

    ✨ React-side implementation of the save logic

--- modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Navigation.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Navigation.js
@@ -2,13 +2,25 @@ import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 import TopNavigation from '../../../ReusableComponents/TopNavigation';
+import { StylingHooks } from '../../../../data';
 
 const SettingsNavigation = () => {
+	const { persist: persistStyling } = StylingHooks.useStore();
+	const isBusy = false; // TODO: Implement loading state.
+
+	const handleSaveClick = () => {
+		persistStyling();
+	};
+
 	const title = __( 'PayPal Payments', 'woocommerce-paypal-payments' );
 
 	return (
 		<TopNavigation title={ title } exitOnTitleClick={ true }>
-			<Button variant="primary" disabled={ false }>
+			<Button
+				variant="primary"
+				disabled={ isBusy }
+				onClick={ handleSaveClick }
+			>
 				{ __( 'Save', 'woocommerce-paypal-payments' ) }
 			</Button>
 		</TopNavigation>
