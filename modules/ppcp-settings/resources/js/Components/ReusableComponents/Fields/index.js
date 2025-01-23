3840352c54b2e34916f41dfbe76a0541019fc9cd
Philipp Stracker <p.stracker@syde.com>
2025-01-23 19:50:13 +0100

    🔥 Remove unused files

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/Fields/index.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/Fields/index.js
@@ -7,6 +7,5 @@ export { default as CheckboxGroup } from './CheckboxGroup';
 export { default as RadioGroup } from './RadioGroup';
 export { default as PayPalRdb } from './RadioButton';
 export { default as PayPalRdbWithContent } from './RadioContent';
-export { default as SelectBox } from './SelectableContent';
 export { default as OptionSelector } from './OptionSelector';
 export { default as Select } from './Select';
