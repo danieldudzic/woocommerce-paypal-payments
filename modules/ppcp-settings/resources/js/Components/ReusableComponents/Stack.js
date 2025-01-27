563f9e5ac1a9379b44716789a7b9bc297a53b062
Philipp Stracker <p.stracker@syde.com>
2025-01-27 13:50:28 +0100

    ✨ Create new VStack component

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/Stack.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/Stack.js
@@ -2,13 +2,15 @@
  * Temporary component, until the experimental VStack/HStack block editor component is stable.
  *
  * @see https://wordpress.github.io/gutenberg/?path=/docs/components-experimental-hstack--docs
+ * @see https://wordpress.github.io/gutenberg/?path=/docs/components-experimental-vstack--docs
  * @file
  */
 import classNames from 'classnames';
 
-export const HStack = ( { className, spacing = 3, children } ) => {
+const Stack = ( { type, className, spacing, children } ) => {
 	const wrapperClass = classNames(
-		'components-flex components-h-stack',
+		'components-flex',
+		`components-${ type }-stack`,
 		className
 	);
 
@@ -20,5 +22,21 @@ export const HStack = ( { className, spacing = 3, children } ) => {
 		<div className={ wrapperClass } style={ styles }>
 			{ children }
 		</div>
+	);
+};
+
+export const HStack = ( { className, spacing = 3, children } ) => {
+	return (
+		<Stack type="h" className={ className } spacing={ spacing }>
+			{ children }
+		</Stack>
+	);
+};
+
+export const VStack = ( { className, spacing = 3, children } ) => {
+	return (
+		<Stack type="v" className={ className } spacing={ spacing }>
+			{ children }
+		</Stack>
 	);
 };
