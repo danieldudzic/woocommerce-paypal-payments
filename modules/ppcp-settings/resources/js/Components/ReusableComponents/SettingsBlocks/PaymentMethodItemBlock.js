c2859e362ad38b9d2685bae71f1c56ad0020333e
Philipp Stracker <p.stracker@syde.com>
2025-01-28 12:42:43 +0100

    🐛 Fix the JS error about “Button not defined”

--- modules/ppcp-settings/resources/js/Components/ReusableComponents/SettingsBlocks/PaymentMethodItemBlock.js
+++ modules/ppcp-settings/resources/js/Components/ReusableComponents/SettingsBlocks/PaymentMethodItemBlock.js
@@ -1,4 +1,5 @@
-import { ToggleControl } from '@wordpress/components';
+import { ToggleControl, Icon, Button } from '@wordpress/components';
+import { cog } from '@wordpress/icons';
 
 import SettingsBlock from '../SettingsBlock';
 import PaymentMethodIcon from '../PaymentMethodIcon';
@@ -13,12 +14,12 @@ const PaymentMethodItemBlock = ( {
 		<SettingsBlock className="ppcp--method-item" separatorAndGap={ false }>
 			<div className="ppcp--method-inner">
 				<div className="ppcp--method-title-wrapper">
-                    { paymentMethod?.icon && (
-                        <PaymentMethodIcon
-                            icons={ [ paymentMethod.icon ] }
-                            type={ paymentMethod.icon }
-                        />
-                    ) }
+					{ paymentMethod?.icon && (
+						<PaymentMethodIcon
+							icons={ [ paymentMethod.icon ] }
+							type={ paymentMethod.icon }
+						/>
+					) }
 					<span className="ppcp--method-title">
 						{ paymentMethod.itemTitle }
 					</span>
