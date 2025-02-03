e7820c40c1b57fde39b82288f008c4e5e73cf546
Philipp Stracker <p.stracker@syde.com>
2025-02-03 19:08:45 +0100

    ✨ Add list of learn-more URLs

--- modules/ppcp-settings/resources/js/Components/Screens/Onboarding/hooks/usePaymentConfig.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Onboarding/hooks/usePaymentConfig.js
@@ -35,6 +35,61 @@ const defaultConfig = {
 	),
 };
 
+const learnMoreLinks = {
+	US: {
+		PayPalCheckout:
+			'https://www.paypal.com/us/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/us/business/accept-payments/checkout/installments',
+		Venmo: 'https://www.paypal.com/us/enterprise/payment-processing/accept-venmo',
+		Crypto: 'https://www.paypal.com/us/digital-wallet/manage-money/crypto',
+		OptionalMethods:
+			'https://www.paypal.com/us/business/accept-payments/checkout/integration#expanded-checkout',
+		Fastlane:
+			'https://www.paypal.com/us/enterprise/payment-processing/guest-checkout',
+	},
+	CA: {
+		PayPalCheckout:
+			'https://www.paypal.com/ca/business/accept-payments/checkout',
+	},
+	GB: {
+		PayPalCheckout:
+			'https://www.paypal.com/uk/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/uk/business/accept-payments/checkout/installments',
+	},
+	FR: {
+		PayPalCheckout:
+			'https://www.paypal.com/fr/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/fr/business/accept-payments/checkout/installments',
+	},
+	ES: {
+		PayPalCheckout:
+			'https://www.paypal.com/es/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/es/business/accept-payments/checkout/installments',
+	},
+	IT: {
+		PayPalCheckout:
+			'https://www.paypal.com/it/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/it/business/accept-payments/checkout/installments',
+	},
+	DE: {
+		PayPalCheckout:
+			'https://www.paypal.com/de/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/de/business/accept-payments/checkout/installments',
+	},
+	AU: {
+		PayPalCheckout:
+			'https://www.paypal.com/au/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/au/business/accept-payments/checkout/installments',
+	},
+};
+
 const countrySpecificConfigs = {
 	US: {
 		includedMethods: [
@@ -79,6 +134,7 @@ export const usePaymentConfig = (
 	return useMemo( () => {
 		const countryConfig = countrySpecificConfigs[ country ] || {};
 		const config = { ...defaultConfig, ...countryConfig };
+		const learnMoreConfig = learnMoreLinks[ country ] || {};
 
 		const includedMethods = filterMethods( config.includedMethods, [
 			( method ) => isPayLater || method.name !== 'PayLater',
@@ -92,6 +148,7 @@ export const usePaymentConfig = (
 			...config,
 			includedMethods,
 			optionalMethods,
+			learnMoreConfig,
 		};
 	}, [ country, isPayLater, useAcdc, isFastlane ] );
 };
