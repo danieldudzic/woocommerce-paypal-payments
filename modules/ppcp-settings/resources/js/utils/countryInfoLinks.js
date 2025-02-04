63f538fd62f1fffdfbeee7e2f945cda5a5eb2b17
Philipp Stracker <p.stracker@syde.com>
2025-02-04 17:53:05 +0100

    ♻️ Extract info links to own config file

---
+++ modules/ppcp-settings/resources/js/utils/countryInfoLinks.js
@@ -0,0 +1,54 @@
+export const learnMoreLinks = {
+	US: {
+		PayPalCheckout:
+			'https://www.paypal.com/us/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/us/business/accept-payments/checkout/installments',
+		Venmo: 'https://www.paypal.com/us/enterprise/payment-processing/accept-venmo',
+		Crypto: 'https://www.paypal.com/us/digital-wallet/manage-money/crypto',
+		OptionalMethods:
+			'https://www.paypal.com/us/business/accept-payments/checkout/integration#expanded-checkout',
+		Fastlane:
+			'https://www.paypal.com/us/enterprise/payment-processing/guest-checkout',
+	},
+	CA: {
+		PayPalCheckout:
+			'https://www.paypal.com/ca/business/accept-payments/checkout',
+	},
+	GB: {
+		PayPalCheckout:
+			'https://www.paypal.com/uk/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/uk/business/accept-payments/checkout/installments',
+	},
+	FR: {
+		PayPalCheckout:
+			'https://www.paypal.com/fr/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/fr/business/accept-payments/checkout/installments',
+	},
+	ES: {
+		PayPalCheckout:
+			'https://www.paypal.com/es/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/es/business/accept-payments/checkout/installments',
+	},
+	IT: {
+		PayPalCheckout:
+			'https://www.paypal.com/it/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/it/business/accept-payments/checkout/installments',
+	},
+	DE: {
+		PayPalCheckout:
+			'https://www.paypal.com/de/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/de/business/accept-payments/checkout/installments',
+	},
+	AU: {
+		PayPalCheckout:
+			'https://www.paypal.com/au/business/accept-payments/checkout',
+		PayLater:
+			'https://www.paypal.com/au/business/accept-payments/checkout/installments',
+	},
+};
