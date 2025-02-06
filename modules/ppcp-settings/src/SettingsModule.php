daf5af28701183666427025cc53c8d49bb0b3d0b
Emili Castells Guasch <e.castells@inpsyde.com>
2025-02-06 11:49:48 +0100

    Add filter for fastlane gateway enabled, title and description

--- modules/ppcp-settings/src/SettingsModule.php
+++ modules/ppcp-settings/src/SettingsModule.php
@@ -411,6 +411,24 @@ class SettingsModule implements ServiceModule, ExecutableModule {
 			2
 		);
 
+		add_filter( 'woocommerce_paypal_payments_axo_gateway_should_update_enabled', '__return_false' );
+		add_filter(
+			'woocommerce_paypal_payments_axo_gateway_title',
+			function( string $title, WC_Payment_Gateway $gateway ) {
+				return $gateway->get_option( 'title', $title );
+			},
+			10,
+			2
+		);
+		add_filter(
+			'woocommerce_paypal_payments_axo_gateway_description',
+			function( string $description, WC_Payment_Gateway $gateway ) {
+				return $gateway->get_option( 'description', $description );
+			},
+			10,
+			2
+		);
+
 		return true;
 	}
 
