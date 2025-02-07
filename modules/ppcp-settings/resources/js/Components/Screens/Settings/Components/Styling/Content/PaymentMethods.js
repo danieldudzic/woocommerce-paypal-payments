7ebf94a600426a5179b6781bb9f898a4145c1ce8
Emili Castells Guasch <e.castells@inpsyde.com>
2025-02-07 16:03:26 +0100

    Filter enabled payment methods

--- modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Styling/Content/PaymentMethods.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Styling/Content/PaymentMethods.js
@@ -1,17 +1,29 @@
 import { __ } from '@wordpress/i18n';
 
-import { StylingHooks } from '../../../../../../data';
+import { PaymentHooks, StylingHooks } from '../../../../../../data';
 import { CheckboxStylingSection } from '../Layout';
 
 const SectionPaymentMethods = ( { location } ) => {
 	const { paymentMethods, setPaymentMethods, choices } =
 		StylingHooks.usePaymentMethodProps( location );
 
+	const methods = PaymentHooks.usePaymentMethods();
+	const methodIds = [];
+	methods.all.forEach( ( method ) => {
+		if ( method.enabled === true ) {
+			methodIds.push( method.id );
+		}
+	} );
+
+	const filteredChoices = choices.filter( ( choice ) => {
+		return methodIds.includes( choice.paymentMethod );
+	} );
+
 	return (
 		<CheckboxStylingSection
 			name="payment-methods"
 			title={ __( 'Payment Methods', 'woocommerce-paypal-payments' ) }
-			options={ choices }
+			options={ filteredChoices }
 			value={ paymentMethods }
 			onChange={ setPaymentMethods }
 		/>
