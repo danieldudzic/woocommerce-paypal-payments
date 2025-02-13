16ec522830f28dadd495597ff0fb0ec0d4c5d4fe
Philipp Stracker <p.stracker@syde.com>
2025-02-13 15:27:29 +0100

    ⚡️ Memoize payment methods in styling tab

--- modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Styling/Content/PaymentMethods.js
+++ modules/ppcp-settings/resources/js/Components/Screens/Settings/Components/Styling/Content/PaymentMethods.js
@@ -2,17 +2,21 @@ import { __ } from '@wordpress/i18n';
 
 import { PaymentHooks, StylingHooks } from '../../../../../../data';
 import { CheckboxStylingSection } from '../Layout';
+import { useMemo } from '@wordpress/element';
 
 const SectionPaymentMethods = ( { location } ) => {
 	const { paymentMethods, setPaymentMethods, choices } =
 		StylingHooks.usePaymentMethodProps( location );
-
 	const { all: allMethods } = PaymentHooks.usePaymentMethods();
 
-	const filteredChoices = choices.filter( ( choice ) => {
-		const methodConfig = allMethods.find( ( i ) => i.id === choice.value );
-		return methodConfig?.enabled;
-	} );
+	const filteredChoices = useMemo( () => {
+		return choices.filter( ( choice ) => {
+			const methodConfig = allMethods.find(
+				( i ) => i.id === choice.value
+			);
+			return methodConfig?.enabled;
+		} );
+	}, [ choices, allMethods ] );
 
 	return (
 		<CheckboxStylingSection
