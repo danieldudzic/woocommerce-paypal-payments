a16ad7fef5ef75caef7c014e4acb806f14396fcc
Narek Zakarian <narekzakarian@gmail.com>
2025-02-20 13:38:35 +0400

    improve the wording.

--- modules/ppcp-compat/src/Settings/SubscriptionSettingsMapHelper.php
+++ modules/ppcp-compat/src/Settings/SubscriptionSettingsMapHelper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * A helper for mapping the old/new subscription settings.
+ * A helper for mapping old and new subscription settings.
  *
  * @package WooCommerce\PayPalCommerce\Compat\Settings
  */
@@ -12,13 +12,13 @@ namespace WooCommerce\PayPalCommerce\Compat\Settings;
 use WooCommerce\PayPalCommerce\WcSubscriptions\Helper\SubscriptionHelper;
 
 /**
- * A map of old to new styling settings.
+ * Handles mapping between old and new subscription settings.
  *
- * In new settings UI we have to automatically set Subscriptions mode value based on the type of merchant.
- * So here we will fake the map and later inject the value of the old subscription setting value based on the type of merchant.
- * - Non-vaulting merchants should have it set to PayPal Subscriptions.
- * - Merchants with vaulting should have it set to PayPal Vaulting.
- * - The disabled subscriptions can be retrieved by using a filter.
+ * In the new settings UI, the Subscriptions mode value is set automatically based on the merchant type.
+ * This class fakes the mapping and injects the appropriate value based on the merchant:
+ * - Non-vaulting merchants will use PayPal Subscriptions.
+ * - Merchants with vaulting will use PayPal Vaulting.
+ * - Disabled subscriptions can be controlled using a filter.
  *
  * @psalm-import-type newSettingsKey from SettingsMap
  * @psalm-import-type oldSettingsKey from SettingsMap
@@ -48,8 +48,8 @@ class SubscriptionSettingsMapHelper {
 	/**
 	 * Maps the old subscription setting key.
 	 *
-	 * We just need to fake the map here as this setting doesn't exist it new settings.
-	 * We will automatically set Subscriptions mode value based on the type of merchant.
+	 * This method creates a placeholder mapping as this setting doesn't exist in the new settings.
+	 * The Subscriptions mode value is set automatically based on the merchant type.
 	 *
 	 * @psalm-return array<oldSettingsKey, newSettingsKey>
 	 */
@@ -58,12 +58,12 @@ class SubscriptionSettingsMapHelper {
 	}
 
 	/**
-	 * Retrieves the value of a mapped subscriptions_mode key from the new settings.
+	 * Retrieves the mapped value for the subscriptions_mode key from the new settings.
 	 *
 	 * @param string                      $old_key The key from the legacy settings.
 	 * @param array<string, scalar|array> $settings_model The new settings model data as an array.
 	 *
-	 * @return 'vaulting_api'|'subscriptions_api'|'disable_paypal_subscriptions'|null The value of the mapped subscriptions_mode setting (null if not found).
+	 * @return 'vaulting_api'|'subscriptions_api'|'disable_paypal_subscriptions'|null The mapped subscriptions_mode value, or null if not applicable.
 	 */
 	public function mapped_value( string $old_key, array $settings_model ): ?string {
 		if ( $old_key !== 'subscriptions_mode' || ! $this->subscription_helper->plugin_is_active() ) {
@@ -74,9 +74,9 @@ class SubscriptionSettingsMapHelper {
 		$subscription_mode_value = $vaulting ? self::OLD_SETTINGS_SUBSCRIPTION_MODE_VALUE_VAULTING : self::OLD_SETTINGS_SUBSCRIPTION_MODE_VALUE_SUBSCRIPTIONS;
 
 		/**
-		 * Allows to disable the subscription mode when using the new settings UI.
+		 * Allows disabling the subscription mode when using the new settings UI.
 		 *
-		 * @returns bool true if the subscription mode should be disabled, otherwise false (also by default).
+		 * @returns bool true if the subscription mode should be disabled, false otherwise (default is false).
 		 */
 		$subscription_mode_disabled = (bool) apply_filters( 'woocommerce_paypal_payments_subscription_mode_disabled', false );
 
