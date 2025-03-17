b6c62ba593016aac0466ad89b6b4ffe447b702f9
carmenmaymo <mayen22@gmail.com>
2025-03-17 11:21:13 +0100

    Move enqueue settings scripts to a class

--- modules/ppcp-settings/src/SettingsModule.php
+++ modules/ppcp-settings/src/SettingsModule.php
@@ -39,7 +39,6 @@ use WooCommerce\PayPalCommerce\WcGateway\Gateway\OXXO\OXXO;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayPalGateway;
 use WooCommerce\PayPalCommerce\WcGateway\Gateway\PayUponInvoice\PayUponInvoiceGateway;
 use WooCommerce\PayPalCommerce\WcGateway\Helper\DCCProductStatus;
-use WooCommerce\PayPalCommerce\WcGateway\Settings\Settings;
 use WooCommerce\PayPalCommerce\Settings\Service\SettingsDataManager;
 use WooCommerce\PayPalCommerce\Settings\DTO\ConfigurationFlagsDTO;
 use WooCommerce\PayPalCommerce\Settings\Enum\ProductChoicesEnum;
@@ -152,98 +151,9 @@ class SettingsModule implements ServiceModule, ExecutableModule {
 
 		add_action(
 			'admin_enqueue_scripts',
-			/**
-			 * Param types removed to avoid third-party issues.
-			 *
-			 * @psalm-suppress MissingClosureParamType
-			 */
-			static function ( $hook_suffix ) use ( $container ) {
-				if ( 'woocommerce_page_wc-settings' !== $hook_suffix ) {
-					return;
-				}
-
-				/**
-				 * Require resolves.
-				 *
-				 * @psalm-suppress UnresolvableInclude
-				 */
-				$script_asset_file = require dirname( realpath( __FILE__ ) ?: '', 2 ) . '/assets/index.asset.php';
-
-				$module_url = $container->get( 'settings.url' );
-
-				wp_register_script(
-					'ppcp-admin-settings',
-					$module_url . '/assets/index.js',
-					$script_asset_file['dependencies'],
-					$script_asset_file['version'],
-					true
-				);
-
-				wp_enqueue_script( 'ppcp-admin-settings', '', array( 'wp-i18n' ), $script_asset_file['version'] );
-				wp_set_script_translations(
-					'ppcp-admin-settings',
-					'woocommerce-paypal-payments',
-				);
-
-				/**
-				 * Require resolves.
-				 *
-				 * @psalm-suppress UnresolvableInclude
-				 */
-				$style_asset_file = require dirname( realpath( __FILE__ ) ?: '', 2 ) . '/assets/style.asset.php';
-
-				wp_register_style(
-					'ppcp-admin-settings',
-					$module_url . '/assets/style-style.css',
-					$style_asset_file['dependencies'],
-					$style_asset_file['version']
-				);
-
-				$settings = $container->get( 'wcgateway.settings' );
-				assert( $settings instanceof Settings );
-
-				wp_enqueue_style( 'ppcp-admin-settings' );
-
-				wp_enqueue_style( 'ppcp-admin-settings-font', 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap', array(), $style_asset_file['version'] );
-
-				$is_pay_later_configurator_available = $container->get( 'paylater-configurator.is-available' );
-
-				$script_data = array(
-					'assets'                          => array(
-						'imagesUrl' => $module_url . '/images/',
-					),
-					'wcPaymentsTabUrl'                => admin_url( 'admin.php?page=wc-settings&tab=checkout' ),
-					'pluginSettingsUrl'               => admin_url( 'admin.php?page=wc-settings&tab=checkout&section=ppcp-gateway' ),
-					'debug'                           => defined( 'WP_DEBUG' ) && WP_DEBUG,
-					'isPayLaterConfiguratorAvailable' => $is_pay_later_configurator_available,
-					'storeCountry'                    => $container->get( 'wcgateway.store-country' ),
-				);
-
-				if ( $is_pay_later_configurator_available ) {
-					wp_enqueue_script(
-						'ppcp-paylater-configurator-lib',
-						'https://www.paypalobjects.com/merchant-library/merchant-configurator.js',
-						array( 'wp-i18n' ),
-						$script_asset_file['version'],
-						true
-					);
-					wp_set_script_translations(
-						'ppcp-paylater-configurator-lib',
-						'woocommerce-paypal-payments',
-					);
-					$script_data['PcpPayLaterConfigurator'] = array(
-						'config'           => array(),
-						'merchantClientId' => $settings->get( 'client_id' ),
-						'partnerClientId'  => $container->get( 'api.partner_merchant_id' ),
-						'bnCode'           => PPCP_PAYPAL_BN_CODE,
-					);
-				}
-
-				wp_localize_script(
-					'ppcp-admin-settings',
-					'ppcpSettings',
-					$script_data
-				);
+			function ( $hook_suffix ) use ( $container ) : void {
+				$script_data_handler = $container->get( 'settings.service.script-data-handler' );
+				$script_data_handler->localize_scripts( $hook_suffix );
 			}
 		);
 
