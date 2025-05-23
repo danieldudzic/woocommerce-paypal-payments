526f8944f34e6ec84eae3959acd5974f64fc3899
Daniel Dudzic <d.dudzic@syde.com>
2025-05-23 19:25:38 +0200

    ⚙️ Add Configure button for the Installments feature

--- modules/ppcp-settings/src/Data/Definition/FeaturesDefinition.php
+++ modules/ppcp-settings/src/Data/Definition/FeaturesDefinition.php
@@ -331,11 +331,18 @@ class FeaturesDefinition {
 				'buttons'     => array(
 					array(
 						'type'     => 'secondary',
+						'text'     => __( 'Configure', 'woocommerce-paypal-payments' ),
+						'url'      => 'https://www.paypal.com/businessmanage/preferences/installmentplan',
+						'showWhen' => 'enabled',
+						'class'    => 'small-button',
+					),
+					array(
+						'type'     => 'secondary',
 						'text'     => __( 'Sign up', 'woocommerce-paypal-payments' ),
 						'url'      => 'https://www.paypal.com/businessmanage/preferences/installmentplan',
 						'showWhen' => 'disabled',
 						'class'    => 'small-button',
-					),
+					)
 				),
 			),
 		);
