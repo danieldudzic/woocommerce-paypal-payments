532b083f036102835ec11b4c94e7869b07b831ce
Emili Castells Guasch <e.castells@inpsyde.com>
2025-06-20 17:17:33 +0200

    Fix phpcs

--- modules/ppcp-wc-gateway/src/Gateway/CreditCardGateway.php
+++ modules/ppcp-wc-gateway/src/Gateway/CreditCardGateway.php
@@ -460,13 +460,13 @@ class CreditCardGateway extends \WC_Payment_Gateway_CC {
 			$tokens = WC_Payment_Tokens::get_customer_tokens( get_current_user_id() );
 			foreach ( $tokens as $token ) {
 				if ( $token->get_id() === (int) $card_payment_token_id ) {
-					$custom_id    = (string) $wc_order->get_id();
-					$invoice_id   = $this->prefix . $wc_order->get_order_number();
+					$custom_id  = (string) $wc_order->get_id();
+					$invoice_id = $this->prefix . $wc_order->get_order_number();
 
 					try {
 						$create_order = $this->capture_card_payment->create_order( $token->get_token(), $custom_id, $invoice_id, $wc_order );
-					} catch (RuntimeException $exception) {
-						$this->logger->error($exception->getMessage());
+					} catch ( RuntimeException $exception ) {
+						$this->logger->error( $exception->getMessage() );
 					}
 
 					$order = $this->order_endpoint->order( $create_order->id );
