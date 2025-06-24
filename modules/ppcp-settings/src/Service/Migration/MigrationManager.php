6af9e99fa3c6fca71e8b693b869c762d086ea5df
Narek Zakarian <narekzakarian@gmail.com>
2025-06-24 13:37:40 +0400

    Create migration manager

---
+++ modules/ppcp-settings/src/Service/Migration/MigrationManager.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Provides functionality for settings migration management.
+ *
+ * @package WooCommerce\PayPalCommerce\Settings\Service\Migration
+ */
+
+declare( strict_types = 1 );
+
+namespace WooCommerce\PayPalCommerce\Settings\Service\Migration;
+
+/**
+ * Class MigrationManager
+ *
+ * Manages migration operations for plugin settings.
+ */
+class MigrationManager {
+
+	public const OPTION_NAME_IS_MIGRATION_RUNNING = 'woocommerce_ppcp-settings-is-migration-running';
+
+	protected GeneralSettingsMigration $general_settings_migration;
+	protected SettingsTabMigration $settings_tab_migration;
+	protected StylingSettingsMigration $styling_settings_migration;
+	protected PaymentSettingsMigration $payment_settings_migration;
+
+	public function __construct(
+		GeneralSettingsMigration $general_settings_migration,
+		SettingsTabMigration $settings_tab_migration,
+		StylingSettingsMigration $styling_settings_migration,
+		PaymentSettingsMigration $payment_settings_migration
+	) {
+		$this->general_settings_migration = $general_settings_migration;
+		$this->settings_tab_migration = $settings_tab_migration;
+		$this->styling_settings_migration = $styling_settings_migration;
+		$this->payment_settings_migration = $payment_settings_migration;
+	}
+
+	/**
+	 * Executes migration for all settings areas/tabs.
+	 *
+	 * @return void
+	 */
+	public function migrate(): void {
+		$this->general_settings_migration->migrate();
+		$this->settings_tab_migration->migrate();
+		$this->styling_settings_migration->migrate();
+		$this->payment_settings_migration->migrate();
+	}
+}
